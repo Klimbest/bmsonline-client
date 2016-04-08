278366af06e7892bb0a0d835b394b64f4c06ac43
Paweł <pawelzajder@gmail.com>
2016-04-08 10:59:40 +0200

    55

--- web/js/tmpBmsDataAnalyze.js
+++ web/js/tmpBmsDataAnalyze.js
@@ -1,6 +1,6 @@
 /* global registersToChart */
 
-registersToChart = [[4, 1]];
+registersToChart = [[5, 1]];
 $(document).ready(function () {
     var mchart = $('#masterContainer').highcharts();
     var dchart = $('#detailContainer').highcharts();
