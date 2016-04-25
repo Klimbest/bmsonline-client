d8aa4eedc3d9bd9a009e8a5cfcdb6ffd2d4b767f
Paweł <pawelzajder@gmail.com>
2016-04-25 14:39:17 +0200

    aaaaa

--- web/js/tmpBms.js
+++ web/js/tmpBms.js
@@ -93,6 +93,7 @@ function ajaxRefreshPage(terms) {
             if (value != null) {                
                 var displayPrecision = parseInt($("div.bms-panel-variable").children("span#" + key).attr("value"));
                 roundValue = parseFloat(value).toFixed(displayPrecision);
+                console.log(roundValue);
             }
             $("div.bms-panel").children("span#" + key).empty().append(roundValue);
             
