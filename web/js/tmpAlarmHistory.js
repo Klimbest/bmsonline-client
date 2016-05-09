6dce6cd035fbb1a33910d12adc064086ec32309b
Paweł <pawelzajder@gmail.com>
2016-05-09 15:27:40 +0200

    aaa

--- web/js/tmpAlarmHistory.js
+++ web/js/tmpAlarmHistory.js
@@ -1,9 +1,10 @@
 
 $(document).ready(function () {
+    
     $("div.row").find("span.alarmValue").each(function(){
-        var value = parseInt($(this).trim());
+        var value = parseInt($(this).text());
         if(value == 0){
-            $(this).parent().parent().css({color: "#00FF00"});
+            $(this).parent().parent().css({color: "#00AA00"});
         }else{
             $(this).parent().parent().css({color: "#FF0000"});            
         }
