22a22f85c75c1c95ff8365bcb67814bd2cf1720d
Paweł <pawelzajder@gmail.com>
2016-05-13 08:51:59 +0200

    aaaa

--- src/BmsBundle/Controller/DefaultController.php
+++ src/BmsBundle/Controller/DefaultController.php
@@ -15,10 +15,9 @@ class DefaultController extends Controller {
      * @Route("/", name="bms_index")
      */
     public function bmsIndexAction() {
-        
-        $process = new Process('ssh bmsonline@bmsonline.pl ./updateApp.sh tid');
-        $process->run();
-        var_dump($process->getOutput());
+
+//        $process = new Process('bash ../../orderToRPi.sh dbSync');
+//        $process->run();
         return $this->render('BmsBundle::index.html.twig');
     }
 
@@ -30,14 +29,14 @@ class DefaultController extends Controller {
             $pageRepo = $this->getDoctrine()->getRepository('BmsVisualizationBundle:Page');
             $termRepo = $this->getDoctrine()->getRepository('BmsVisualizationBundle:Term');
             $widgetBarRepo = $this->getDoctrine()->getRepository('BmsVisualizationBundle:WidgetBar');
-            
+
             $page_id = $request->get("page_id");
             $pages = $pageRepo->findAll();
             isset($page_id) ? $page = $pageRepo->find($page_id) : $page = $pageRepo->find(2);
-            
-            $terms = $termRepo->findAllAsArray();       
-                     
-            $widgets = $widgetBarRepo->findAll();   
+
+            $terms = $termRepo->findAllAsArray();
+
+            $widgets = $widgetBarRepo->findAll();
             $ret["terms"] = $terms;
             $ret['template'] = $this->container->get('templating')->render('BmsBundle::page.html.twig', ['pages' => $pages, 'page' => $page, 'terms' => $terms, 'widgets' => $widgets]);
             return new JsonResponse($ret);
@@ -58,7 +57,7 @@ class DefaultController extends Controller {
             $widgetBarRepo = $this->getDoctrine()->getRepository('BmsVisualizationBundle:WidgetBar');
             $technicalInformationRepo = $this->getDoctrine()->getRepository('BmsConfigurationBundle:TechnicalInformation');
             $deviceRepo = $this->getDoctrine()->getRepository('BmsConfigurationBundle:Device');
-            
+
             $page_id = $request->get("page_id");
             //get all panels for page
             $panels = $panelRepo->findVariablePanelsForPage($page_id);
@@ -69,14 +68,14 @@ class DefaultController extends Controller {
             }
             //get all terms
             $terms = $termRepo->findAll();
-            foreach ($terms as $t){
+            foreach ($terms as $t) {
                 $register = $t->getRegister();
                 $registers[$register->getId()] = $register->getRegisterCurrentData()->getFixedValue();
             }
             //get all bar widgets
             $widgets = $widgetBarRepo->findAll();
-            foreach ($widgets as $w){
-                if($w->getSetRegisterId() != null){
+            foreach ($widgets as $w) {
+                if ($w->getSetRegisterId() != null) {
                     $register = $w->getSetRegisterId();
                     $registers[$register->getId()] = $register->getRegisterCurrentData()->getFixedValue();
                 }
@@ -84,16 +83,16 @@ class DefaultController extends Controller {
                 $registers[$register->getId()] = $register->getRegisterCurrentData()->getFixedValue();
             }
             $devicesStatus = $technicalInformationRepo->getDevicesStatus();
-            
-            foreach($devicesStatus as &$ds){
-                $devices_id = explode("_", $ds['name']);                
-                $device = $deviceRepo->find((int)$devices_id[1]);
+
+            foreach ($devicesStatus as &$ds) {
+                $devices_id = explode("_", $ds['name']);
+                $device = $deviceRepo->find((int) $devices_id[1]);
                 $ds['name'] = $device->getName();
-            }            
-            
+            }
+
             //get last hello from RPi      
             $time = $technicalInformationRepo->getRpiStatus();
-            
+
             $ret['devicesStatus'] = $devicesStatus;
             $ret['state'] = $time[0]["time"]->getTimestamp();
             $ret['registers'] = $registers;
@@ -102,4 +101,5 @@ class DefaultController extends Controller {
             throw new AccessDeniedHttpException();
         }
     }
+
 }
