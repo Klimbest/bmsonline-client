bf6312fea5aea516569d1def24777b1260a6aa32
Paweł <pawelzajder@gmail.com>
2016-05-25 08:42:00 +0200

    aaa

--- src/BmsAdminBundle/Controller/DefaultController.php
+++ src/BmsAdminBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace BmsAdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller {
 
@@ -11,7 +12,18 @@ class DefaultController extends Controller {
      * @Route("/", name="bms_admin")
      */
     public function indexAction() {
-        return $this->render('BmsAdminBundle:Default:index.html.twig');
+
+        $userManager = $this->get('fos_user.user_manager');
+        
+        $users = $userManager->findUsers();
+
+        return $this->render('BmsAdminBundle::index.html.twig', array('users' => $users));
     }
 
+    public function manageUserAction(Request $request){
+        
+        $form = $this->createForm(PostType::class, $post);
+        
+    }
+    
 }
