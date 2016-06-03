e957600b3679cfb26164784cb38286f6c7a01f80
Paweł <pawelzajder@gmail.com>
2016-06-03 10:53:49 +0200

    aaaaa

--- src/BmsBundle/Controller/DefaultController.php
+++ src/BmsBundle/Controller/DefaultController.php
@@ -43,7 +43,7 @@ class DefaultController extends Controller {
     }
 
      /**
-     * @Route("/token_send", name="bms_token_send")
+     * @Route("/resetting/token_send", name="bms_token_send")
      */
     public function tokenSendAction(){
      return $this->render('BmsBundle:Resetting:tokenSend.html.twig');        
