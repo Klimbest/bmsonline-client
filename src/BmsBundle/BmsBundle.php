1f9766f04b110083843effab683c4df698447ee7
Paweł <pawelzajder@gmail.com>
2016-06-03 11:37:24 +0200

    asdasd

--- src/BmsBundle/BmsBundle.php
+++ src/BmsBundle/BmsBundle.php
@@ -2,19 +2,11 @@
 
 namespace BmsBundle;
 
-use BmsBundle\DependencyInjection\Compiler\FOSUserOverridePass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BmsBundle extends Bundle {
 
     public function getParent() {
         return 'FOSUserBundle';
-    }
-    
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-        $container->addCompilerPass(new FOSUserOverridePass());
     }
 }
