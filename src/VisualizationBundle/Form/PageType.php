e6e0301aed7b2370b127050f08a43fdc3634796d
Paweł <pawelzajder@gmail.com>
2016-08-30 15:07:16 +0200

    [CRUD] Page

---
+++ src/VisualizationBundle/Form/PageType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace VisualizationBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PageType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('width')
+            ->add('height')
+            ->add('backgroundColor')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'VisualizationBundle\Entity\Page'
+        ));
+    }
+}
