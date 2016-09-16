9ace24a465045bdf9701184bc334e26c30b67de6
Paweł <pawelzajder@gmail.com>
2016-09-16 13:17:27 +0200

    chart apperance

--- src/BmsDataAnalyzeBundle/Controller/DataAnalyzeController.php
+++ src/BmsDataAnalyzeBundle/Controller/DataAnalyzeController.php
@@ -9,28 +9,35 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Ob\HighchartsBundle\Highcharts\Highchart;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DataAnalyzeController extends Controller {
+class DataAnalyzeController extends Controller
+{
 
     /**
      * @Route("/", name="bms_data_analyze_index")
      */
-    public function indexAction() {
+    public function indexAction()
+    {
         $registerRepo = $this->getDoctrine()->getRepository('BmsConfigurationBundle:Register');
         $registers = $registerRepo->getArchivedRegisters();
         $detailChart = new Highchart();
         $masterChart = new Highchart();
 
         return $this->render('BmsDataAnalyzeBundle::index.html.twig', ['registers' => $registers,
-                    'detailChart' => $this->generateDetailChart($detailChart),
-                    'masterChart' => $this->generateMasterChart($masterChart)]);
+            'detailChart' => $this->generateDetailChart($detailChart),
+            'masterChart' => $this->generateMasterChart($masterChart)]);
     }
 
-    public function generateDetailChart($detailChart) {
+    public function generateDetailChart($detailChart)
+    {
         $detailChart->global->useUTC(false);
+        $detailChart->colors = ['#2f7ed8', '#ffffff', '#8bbc21', '#910000', '#1aadce', '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'];
         $detailChart->chart->renderTo('detailContainer')
-                ->reflow(true);
+            ->reflow(true)
+            ->backgroundColor("#272b30")
+            ->color("#c8c8c8");
         $detailChart->credits->enabled(false);
-        $detailChart->title->text('Pomiar danych, wykres odświeża się automatycznie');
+        $detailChart->title->text('Pomiar danych, wykres odświeża się automatycznie')
+            ->style(["color" => "#c8c8c8"]);
         $detailChart->xAxis->type('datetime');
         $yAxis = array(
             array(
@@ -88,25 +95,25 @@ class DataAnalyzeController extends Controller {
             )
         );
         $detailChart->yAxis($yAxis);
-        $detailChart->tooltip->pointFormat('<span style="color:{point.color}">O</span> {series.name}: <b>{point.y}</b><br/>')
-                ->crosshairs(true)
-                ->backgroundColor("rgba(255,255,255,0)")
-                ->borderColor("rgba(255,255,255,0)")
-                ->shadow(true)
-                ->shared(true)
-                ->style(['width' => '250px'])
-                ->useHTML(true);
+
+        $detailChart->tooltip
+            ->pointFormat("<span style='color:{point.color}'>&#x25cf;</span> {series.name}: <b>{point.y:.2f}</b><br/>")
+            ->crosshairs(true)
+            ->shared(false)
+            ->style(['width' => '250px'])
+            ->useHTML(true);
         $detailChart->legend->align('right')
-                ->y(25)
-                ->x(-50)
-                ->width(150)
-                ->verticalAlign('top')
-                ->useHTML(true)
-                ->layout('vertical')
-                ->padding(3)
-                ->itemMarginTop(3)
-                ->itemMarginBottom(0)
-                ->itemHoverStyle(['color' => '#5BC0DE']);
+            ->y(25)
+            ->x(-50)
+            ->width(150)
+            ->itemStyle(['color' => '#c8c8c8'])
+            ->verticalAlign('top')
+            ->useHTML(true)
+            ->layout('vertical')
+            ->padding(3)
+            ->itemMarginTop(3)
+            ->itemMarginBottom(0)
+            ->itemHoverStyle(['color' => '#5BC0DE']);
         $detailChart->series();
 //        $detailChart->plotOptions->series([
 //            'marker' => [
@@ -120,21 +127,22 @@ class DataAnalyzeController extends Controller {
         return $detailChart;
     }
 
-    public function generateMasterChart($masterChart) {
+    public function generateMasterChart($masterChart)
+    {
 
         $masterChart->global->useUTC(false);
         $masterChart->chart->renderTo('masterContainer')
-                ->reflow(true)
-                ->borderWidth(0)
-                ->backgroundColor(null)
-                ->marginLeft(65)
-                ->marginRight(50)
-                ->zoomType('x')
-                ->events(null);
+            ->reflow(true)
+            ->borderWidth(0)
+            ->backgroundColor(null)
+            ->marginLeft(65)
+            ->marginRight(50)
+            ->zoomType('x')
+            ->events(null);
         $masterChart->title->text(null);
         $masterChart->xAxis->type('datetime')
-                ->showLastTickLabel(true)
-                ->title(array('text' => null));
+            ->showLastTickLabel(true)
+            ->title(array('text' => null));
         $yAxis = array(
             array(
                 'floor' => 0,
@@ -204,7 +212,8 @@ class DataAnalyzeController extends Controller {
     /**
      * @Route("/add_series", name="bms_data_analyze_add_series", options={"expose"=true})
      */
-    public function addSeriesAction(Request $request) {
+    public function addSeriesAction(Request $request)
+    {
         if ($request->isXmlHttpRequest()) {
             $from = $request->get('from');
             $to = $request->get('to');
