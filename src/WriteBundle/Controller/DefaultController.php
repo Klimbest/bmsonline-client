1757331c057c1233f5cafa1a8a83e2e2b3924cdc
Paweł <pawelzajder@gmail.com>
2016-09-21 14:02:09 +0200

    input button

--- src/WriteBundle/Controller/DefaultController.php
+++ src/WriteBundle/Controller/DefaultController.php
@@ -41,7 +41,7 @@ class DefaultController extends Controller
             $vpn = $this->getParameter('vpn');
 //            $query = "'INSERT INTO register_write_data('register_id', 'value', 'get_to_process', username)
             $query = "INSERT INTO register_write_data(register_id, value, get_to_process, username) VALUES(" . $register_id . ", " . $value . ", 1, " . $this->getUser() . ");";
-            $exe = "ssh pi@" . $vpn . " mysql -u root -p modbus KbScanner -e '" . $query . "'";
+            $exe = "ssh pi@" . $vpn . " mysql -u root -p modbus KbScanner -e \"" . $query . "\"";
             $process = new Process($exe);
 
             $process->run();
